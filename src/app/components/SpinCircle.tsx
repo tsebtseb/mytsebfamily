be81fc40893cd9fc7828e481251b27cda62fd35d
“tsebtseb” <tsetseb.alone@gmal.com>
2025-07-08 23:43:51 +0700

    add spin data

--- src/app/components/SpinCircle.tsx
+++ src/app/components/SpinCircle.tsx
@@ -3,30 +3,82 @@ import { useState, useRef } from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 import ChartDataLabels from "chartjs-plugin-datalabels";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 
 // Register necessary components with ChartJS
 ChartJS.register(ArcElement, Tooltip, Legend, ChartDataLabels);
 
 //https://medium.com/@dissanayakewishwajith/creating-an-interactive-spin-wheel-with-next-js-tailwind-css-chart-js-and-typescript-34989268893a
 // Dataset for the spin wheel
-const datasetValues = [
-  { value: 50, color: "#8f7f8f", label: "🍣 ai" },
-  { value: 30, color: "#f97066", label: "be" },
-  { value: 70, color: "#2e90fa", label: "ci" },
-  { value: 40, color: "#fdb022", label: "de" },
-  { value: 60, color: "#ee46bc", label: "ei" },
-  { value: 20, color: "#854CFF", label: "fe" },
+const foodTypeData = [
+  { value: 100, color: "#fa7275", label: "🍱 Japanese" },
+  { value: 80, color: "#60BA97", label: "🍝 Italian" },
+  { value: 50, color: "#3b82f6", label: "🦀 Seafood" },
+  { value: 40, color: "#f9ab72", label: "🥟 Chinese" },
+  { value: 60, color: "#ee46bc", label: "🥘 Thai" },
+  { value: 50, color: "black", label: "🥩 Steak" },
+  { value: 50, color: "#dc2626", label: "🔥 Grill/BBQ" },
+  { value: 20, color: "#854CFF", label: "🫓 Vietnamese" },
+  { value: 20, color: "#facc15", label: "🌮 Mexican" },
+  { value: 20, color: "#34d399", label: "🥖 French" },
 ];
 
-export default function SpinCircle() {
+const restaurantData = [
+  { value: 30, color: "#333333", label: "🥩 Neil Steak" },
+  { value: 90, color: "#D7263D", label: "🐮 Rengaya" },
+  { value: 70, color: "#EF476F", label: "🍣 Sho japanese" },
+  { value: 70, color: "#90BE6D", label: "🍕 Sala Rossa" },
+  { value: 50, color: "#4DCCBD", label: "🍝 Antonio" },
+  { value: 60, color: "	#264653", label: "🌆 Bull & Bear" },
+  { value: 80, color: "#43AA8B", label: "🍣 123 ICHI NI SAN" },
+  { value: 60, color: "#FF6F59", label: "🍖 Sookdal" },
+];
+
+const mamaOrderData = [
+  { value: 100, color: "#FF6F59", label: "🍜  ก๋วยเตี๋ยวเรือ" },
+  { value: 80, color: "#654321", label: "🐔 ข้าวมันไก่" },
+  { value: 60, color: "#abcdef", label: "🍲 ข้าวต้ม" },
+  { value: 20, color: "#FFC300", label: "🍲 ข้าวต้มแห้ง" },
+  { value: 90, color: "#264653", label: "🍳 ครัวมหาชัย" },
+  { value: 70, color: "	#F77F00", label: "🍛 เขียง" },
+  { value: 20, color: "	#FFA552", label: "🍛 ตุ้งแช่" },
+  { value: 60, color: "	#4DCCBD", label: "🐷 ข้าวหมูกรอบ" },
+  { value: 70, color: "#FFD166", label: "🍜 ลิ้มเหล่าโหงว" },
+  { value: 80, color: "#720026", label: "🍜 ก๋วยเตี๋ยวต้มยำ" },
+  { value: 60, color: "#6CC551", label: "🐓 นิตยาไก่ย่าง" },
+  { value: 60, color: "#56CCF2", label: "🐟 แหลมเจริญ" },
+  { value: 80, color: "#43AA8B", label: "🍲 ข้าวผัด" },
+  { value: 70, color: "#123456", label: "🍲 โจ๊กสามย่าน" },
+  { value: 60, color: "#118AB2", label: "🥘 ถูกและดี" },
+  { value: 80, color: "black", label: "🍗 บอนชอน" },
+];
+
+export default function SpinCircle({
+  dataName,
+}: {
+  dataName: "foodTypeData" | "restaurantData" | "mamaOrderData";
+}) {
   const [spinning, setSpinning] = useState<boolean>(false);
   const [winnerIndex, setWinnerIndex] = useState<number | null>(null);
   const [randomRotation, setRandomRotation] = useState<number>(0);
-  const chartRef = useRef<ChartJS<
-    "doughnut",
-    (number | [number, number] | any | any | any)[],
-    unknown
-  > | null>(null);
+  const chartRef = useRef<ChartJS<"doughnut", number[], unknown> | null>(null);
+
+  const allDatasets = {
+    foodTypeData,
+    restaurantData,
+    mamaOrderData,
+  };
+
+  const datasetValues = allDatasets[dataName] ?? foodTypeData;
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
 
   const [activeSegments, setActiveSegments] = useState<number[]>(
     Array.from({ length: datasetValues.length }, (_, i) => i)
@@ -41,7 +93,7 @@ export default function SpinCircle() {
           (item) => datasetValues[item].color
         ),
         borderColor: activeSegments.map((item) => datasetValues[item].color),
-        cutout: "67%", // Adjusted for a larger gap
+        cutout: "50%", // Adjusted for a larger gap
         rotation: randomRotation,
       },
     ],
@@ -78,6 +130,7 @@ export default function SpinCircle() {
         const actualSegmentIndex = activeSegments[randomIndex];
         setWinnerIndex(actualSegmentIndex);
         setSpinning(false);
+        setIsDialogOpen(true);
       }, 3000);
     }
   };
@@ -86,102 +139,159 @@ export default function SpinCircle() {
   const circleRadius = circleSize / 2;
 
   return (
-    <div className="relative w-[340px] h-[340px] md:w-[410px] md:h-[410px] flex items-center justify-center p-4">
-      <div className="absolute -right-40 top-1/2 -translate-y-1/2 space-y-2">
-        {activeSegments.map((segmentIndex) => (
-          <button
-            key={segmentIndex}
-            onClick={() => handleSegmentClick(segmentIndex)}
-            className="flex items-center gap-2 px-3 py-1 rounded-full hover:bg-gray-100"
-          >
-            <div
-              className="w-4 h-4 rounded-full"
-              style={{ backgroundColor: datasetValues[segmentIndex].color }}
-            />
-            <span className="text-sm font-medium">
-              {datasetValues[segmentIndex].label}
-            </span>
-          </button>
-        ))}
-      </div>
+    <>
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <DialogContent className="sm:max-w-md">
+          <DialogHeader>
+            <DialogTitle className="my-2">🎉 Chosen for You!</DialogTitle>
+            <DialogDescription className="flex flex-row justify-center">
+              {winnerIndex !== null && (
+                <span
+                  className="font-popping font-semibold text-lg"
+                  style={{ color: datasetValues[winnerIndex].color }}
+                >
+                  {datasetValues[winnerIndex].label}
+                </span>
+              )}
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex items-center gap-2">
+            <div className="grid max-h-[70vh] overflow-y-scroll flex-1 gap-2">
+              <p>
+                Enjoy our lovely meal ⸜(｡˃ ᵕ ˂ )⸝♡🍚
+                ขอให้กินข้าวให้อิ่มอร่อยๆค่ะ มีความสุขกับการทานอาหารนะคะ
+              </p>
+              <p className="text-muted-foreground">
+                (อะไรก็ได้ที่แปลว่าอะไรก็ได้จริงๆค่ะ)
+              </p>
+            </div>
+          </div>
+          <DialogFooter className="sm:justify-end">
+            <DialogClose asChild>
+              <button className="rounded-full bg-warm-pink px-6 py-2 font-semibold text-white transition-all hover:bg-white hover:text-black active:bg-white active:text-black">
+                Close
+              </button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
 
-      <button
-        onClick={handleSpin}
-        disabled={spinning}
-        style={{
-          backgroundColor:
-            winnerIndex !== null
-              ? datasetValues[winnerIndex].color
-              : "var(--warm-pink)",
-        }}
-        className={`absolute z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  text-white rounded-full p-4 shadow-lg
+      <div className="flex flex-col">
+        <div className="relative w-[340px] h-[340px] md:w-[410px] md:h-[410px] flex items-center justify-center p-4">
+          <div className="absolute -right-40 top-1/2 -translate-y-1/2 space-y-2 hidden md:block">
+            {activeSegments.map((segmentIndex) => (
+              <button
+                key={segmentIndex}
+                onClick={() => handleSegmentClick(segmentIndex)}
+                className="flex items-center gap-2 px-3 py-1 rounded-full hover:bg-gray-100"
+              >
+                <div
+                  className="w-4 h-4 rounded-full"
+                  style={{ backgroundColor: datasetValues[segmentIndex].color }}
+                />
+                <span className="text-sm font-medium">
+                  {datasetValues[segmentIndex].label}
+                </span>
+              </button>
+            ))}
+          </div>
+
+          <button
+            onClick={handleSpin}
+            disabled={spinning}
+            style={{
+              backgroundColor:
+                winnerIndex !== null
+                  ? datasetValues[winnerIndex].color
+                  : "var(--warm-pink)",
+            }}
+            className={`absolute z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  text-white rounded-full p-4 shadow-lg
                 transition-all duration-200 ${
                   spinning
                     ? "opacity-50 cursor-not-allowed"
                     : "cursor-pointer hover:scale-105"
                 }`}
-      >
-        {spinning ? (
-          <span className="animate-pulse">Spinning...</span>
-        ) : winnerIndex !== null ? (
-          <span className="font-semibold">
-            {datasetValues[winnerIndex].label}
-          </span>
-        ) : (
-          <span className="font-semibold">Spin!</span>
-        )}
-      </button>
-
-      <div className="relative w-[300px] h-[300px] md:w-[368px] md:h-[368px] p-4">
-        <Doughnut
-          data={data}
-          options={{
-            plugins: {
-              legend: { display: false },
-              datalabels: {
-                color: "white",
-                font: {
-                  size: 14,
-                  weight: "normal",
-                },
-                formatter: function (_, context) {
-                  return data.labels[context.dataIndex];
-                },
-              },
-            },
-            rotation: -90,
-          }}
-          ref={chartRef}
-        />
-        <svg
-          className="absolute top-0 left-0 w-[300px] h-[300px] md:w-[368px] md:h-[368px] pointer-events-none -rotate-90"
-          viewBox={`0 0 ${circleSize + 11} ${circleSize + 11}`}
-        >
-          <circle
-            className="fill-none stroke-white"
-            cx="50%"
-            cy="50%"
-            r={circleRadius}
-            strokeWidth="2"
-          />
-        </svg>
-        <div
-          id="arrow-icon"
-          className="absolute top-1.5 md:top-[7px] left-1/2 transform -translate-x-1/2 text-white rotate-180"
-        >
-          <svg
-            className="h-7 w-7 text-white z-20"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            fill="white"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
           >
-            <polygon points="12 2 22 22 2 22"></polygon>
-          </svg>
+            {spinning ? (
+              <span className="animate-pulse">Spinning...</span>
+            ) : winnerIndex !== null ? (
+              <span className="font-semibold">
+                {datasetValues[winnerIndex].label}
+              </span>
+            ) : (
+              <span className="font-semibold">Spin!</span>
+            )}
+          </button>
+
+          <div className="relative w-[300px] h-[300px] md:w-[368px] md:h-[368px] p-4">
+            <Doughnut
+              data={data}
+              options={{
+                plugins: {
+                  legend: { display: false },
+                  datalabels: {
+                    color: "white",
+                    font: {
+                      size: 14,
+                      weight: "normal",
+                    },
+                    formatter: function (_, context) {
+                      return data.labels[context.dataIndex];
+                    },
+                  },
+                },
+                rotation: -90,
+              }}
+              ref={chartRef}
+            />
+            <svg
+              className="absolute top-0 left-0 w-[300px] h-[300px] md:w-[368px] md:h-[368px] pointer-events-none -rotate-90"
+              viewBox={`0 0 ${circleSize + 11} ${circleSize + 11}`}
+            >
+              <circle
+                className="fill-none stroke-white"
+                cx="50%"
+                cy="50%"
+                r={circleRadius}
+                strokeWidth="2"
+              />
+            </svg>
+            <div
+              id="arrow-icon"
+              className="absolute top-1.5 md:top-[7px] left-1/2 transform -translate-x-1/2 text-white rotate-180"
+            >
+              <svg
+                className="h-7 w-7 text-white z-20"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                fill="white"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <polygon points="12 2 22 22 2 22"></polygon>
+              </svg>
+            </div>
+          </div>
         </div>
+        <div className="block md:hidden">
+            {activeSegments.map((segmentIndex) => (
+              <button
+                key={segmentIndex}
+                onClick={() => handleSegmentClick(segmentIndex)}
+                className="flex items-center gap-2 px-3 py-1 rounded-full hover:bg-gray-100"
+              >
+                <div
+                  className="w-4 h-4 rounded-full"
+                  style={{ backgroundColor: datasetValues[segmentIndex].color }}
+                />
+                <span className="text-sm font-medium">
+                  {datasetValues[segmentIndex].label}
+                </span>
+              </button>
+            ))}
+          </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/app/ginkhao/page.tsx
+++ src/app/ginkhao/page.tsx
@@ -4,25 +4,31 @@ import Image from "next/image";
 import SpinCircle from "../components/SpinCircle";
 export default function GinKhao() {
   return (
-    <div className="w-full h-full flex flex-col p-5">
-        <div className="flex flex-row gap-x-2 items-center">
-          <div className="w-20 h-20 overflow-hidden">
+    <div className="w-full h-full flex flex-col p-5 overflow-y-scroll">
+      <div className="flex flex-row gap-x-2 items-center">
+        <div className="w-20 h-20 overflow-hidden">
           <Image src={EatGin} alt="" width={350} height={350} />
-          </div>
-          <h1 className="text-4xl font-bold">Dinner Time 🥘</h1>
         </div>
-        <p className="text-muted-foreground my-2">กินอะไรดี?</p>
-        <div className="flex flex-col">
-          <div className="flex flex-row justify-center">
-            <h1 className="text-xl font-semibold">Dinner Time 🥘</h1>
-          </div>
-          <SpinCircle/>
-          
-          <div className="flex flex-row justify-center">
-            <h1 className="text-xl font-semibold">Dinner Time 🥘</h1>
-          </div>
-          <SpinCircle/>
+        <h1 className="text-4xl font-bold">Dinner Time 🍽️</h1>
+      </div>
+      <p className="text-muted-foreground my-2">
+        กินอะไรดี? ദ്ദി(˵ •̀ ᴗ - ˵ ) ✧
+      </p>
+      <div className="flex flex-col gap-y-2 items-center">
+        <h1 className="text-xl font-semibold">🥘 Dinner Time (๑ᵔ⤙ᵔ๑)</h1>
+        <p className="text-muted-foreground">หมวดหมู่อาหาร</p>
+        <SpinCircle dataName="foodTypeData" />
+
+        <h1 className="text-xl font-semibold">👨‍🍳 Restaurants</h1>
+        <p className="text-muted-foreground">ลิสต์ร้านอาหาร</p>
+
+        <SpinCircle dataName="restaurantData" />
+
+        <h1 className="text-xl font-semibold">💖 Mommy's Pick!</h1>
+        <div className="lg:my-20 md:my-15 sm:my-4">
+          <SpinCircle dataName="mamaOrderData" />
         </div>
+      </div>
     </div>
   );
 }
